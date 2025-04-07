9dbfdeb95f49aa46df887b01abefc71502b59814
Nathan-Gui <nathan.std@icloud.com>
2025-04-07 16:14:27 -0600

    Update server.ts
    disable cors

--- tsx-backend/server.ts
+++ tsx-backend/server.ts
@@ -4,11 +4,11 @@ export {}
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 // What is cors for
 // Cross-Origin Resource Sharing (CORS) is a security feature implemented in browsers to prevent JavaScript code from making requests to a different domain than the one that served the JavaScript code. This is a security feature to prevent malicious websites from making requests to other websites on behalf of the user. When you enable CORS in your server, you allow your server to accept requests from other domains. This is useful when you have a frontend application that makes requests to a backend server. By enabling CORS, you allow the frontend application to make requests to the backend server.
 
